3ce9f504445326be16663ce7a3ee49f5c6519dcf
Klayton <kliekkio@gmail.com>
2017-08-31 13:54:08 -0700

    working twitch API with clickable links, stream names and thumbnails

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+  // empty array for holding the games that we search
 var gamesArray = [];
 
+  // submit button to search the twitch API for whatever is inputed into search input box
 $("#submit-button").on("click", function(e) {
       e.preventDefault();
       var game = $("#game-search").val().trim();
       console.log(game);
       var queryURL = "https://api.twitch.tv/kraken/streams/?game=" + game + "&limit=10";
           
+            // pushing the games search into the empty array and console logging it
           gamesArray.push(game);
           console.log(gamesArray);
 
@@ -19,7 +22,7 @@ $("#submit-button").on("click", function(e) {
 
         var results = response.streams;
         
-
+          // for loop to search through the 10 results
         for (var i = 0; i < results.length; i++) {
 
           var streamDiv = $("<div class='streamDiv'>");
@@ -27,19 +30,22 @@ $("#submit-button").on("click", function(e) {
 
           var streamName = results[i].channel.display_name;
           var streamLink = results[i].channel.url;
+          
+            // making tags for the streamName and streamLink
+          var h5 = $("<h5>").text("Streamer name: " + streamName);                  
+          var p = $("<p>").html("Link: " + "<a href="+streamLink+">"+streamLink+"</a>");
 
-          var h5 = $("<h5>").text("Streamer name: " + streamName);
-          var p = $("<p>").text("Link: " + streamLink);
-
+            // making an img div for the thumbnail to the stream
           var streamImage = $("<img>");
 
           streamImage.attr("src", results[i].preview.medium);
 
+            // appending the streamDiv for thumbnail, name and link
           streamDiv.append(h5);
           streamDiv.append(p);
           streamDiv.append(streamImage);
 
-
+            // prepending all of the results into the results-display div
           $("#results-display").prepend(streamDiv);
 
         }
